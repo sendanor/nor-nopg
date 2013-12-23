831e2e87b9d6c8818b22ffde86dc0269584be0de
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-12-23 21:53:40 +0200

    Updated

--- tests/test-nopg.js
+++ tests/test-nopg.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var pgconfig = process.env.PGCONFIG || 'postgres://test:1234567@localhost/test';
+
 /* */
 describe('nopg', function(){
 
@@ -13,7 +15,24 @@ describe('nopg', function(){
 
 	describe('.start', function(){
 
-		it('is callable', function(){ assert.strictEqual(typeof nopg.start, 'function'); });
+		it('is callable', function(){
+			assert.strictEqual(typeof nopg.start, 'function');
+		});
+
+	});
+
+	describe('tests', function(){
+
+		it('.create({"hello":"world"}) works', function(done){
+			nopg.start(pgconfig).create({"hello":"world"}).commit().then(function(db) {
+				done();
+			}).fail(function(err) {
+				done(err);
+			}).done();
+		});
+
+		afterEach(function(done){
+		});
 
 	});
 
