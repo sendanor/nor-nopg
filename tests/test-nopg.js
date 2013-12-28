c3319e0c237de1cd4eb8f0d7d1878207c04a416c
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-12-28 22:12:57 +0200

    Fixed tests

--- tests/test-nopg.js
+++ tests/test-nopg.js
@@ -4,6 +4,20 @@ var util = require('util');
 var PGCONFIG = process.env.PGCONFIG || 'postgres://test:1234567@localhost/test';
 var debug = require('nor-debug');
 
+/** Run init() at start */
+before(function(done){
+	var nopg = require('../src');
+	nopg.start(PGCONFIG).init().then(function(db) {
+		var doc = db.fetch();
+		util.debug('initialized database: doc = ' + util.inspect(doc));
+		return db.commit();
+	}).then(function(db) {
+		done();
+	}).fail(function(err) {
+		done(err);
+	}).done();
+});
+
 /* */
 describe('nopg', function(){
 
@@ -12,7 +26,6 @@ describe('nopg', function(){
 
 	var is = require('nor-is');
 	var assert = require('assert');
-
 	var nopg = require('../src');
 
 	describe('.start', function(){
@@ -24,19 +37,6 @@ describe('nopg', function(){
 	});
 
 	describe('tests', function() {
-
-		/** Run init() at start */
-		before(function(done){
-			nopg.start(PGCONFIG).init().then(function(db) {
-				var doc = db.fetch();
-				util.debug('initialized database: doc = ' + util.inspect(doc));
-				return db.commit();
-			}).then(function(db) {
-				done();
-			}).fail(function(err) {
-				done(err);
-			}).done();
-		});
 
 		it('.create()({"hello":"world"}) works', function(done){
 			nopg.start(PGCONFIG).create()({"hello":"world"}).then(function(db) {
