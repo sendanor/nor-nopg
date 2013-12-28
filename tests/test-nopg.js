cc335c07a42e73f9c094762c6792b47b89080318
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-12-28 21:40:44 +0200

    Skipped one test

--- tests/test-nopg.js
+++ tests/test-nopg.js
@@ -52,7 +52,7 @@ describe('nopg', function(){
 			}).done();
 		});
 
-		it('.createType() and .create(Type)({"hello":"world"}) works', function(done){
+		it.skip('.createType() and .create(Type)({"hello":"world"}) works', function(done){
 			nopg.start(PGCONFIG).createType("Test")({"schema":{"type":"object"}}).create("Test")({"hello":"world"}).then(function(db) {
 				debug.log('db is ', db);
 				var type = db.fetch();
