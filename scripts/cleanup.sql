b9a19ab265df1106429a2c8489e8b3edc0b8601b
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-01-04 22:30:16 +0200

    Added cleanup SQL script

---
+++ scripts/cleanup.sql
@@ -0,0 +1,12 @@
+-- 
+-- Deletes everything, do not use in production!
+-- 
+
+DROP TABLE IF EXISTS dbversion;
+DROP TABLE IF EXISTS attachments;
+DROP TABLE IF EXISTS libs;
+DROP TABLE IF EXISTS documents;
+DROP TABLE IF EXISTS objects;
+DROP TABLE IF EXISTS types;
+DROP FUNCTION tv4.validateresult(json,json);
+DROP SCHEMA tv4;
