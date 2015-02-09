66361d0bc5315cb6be94a3af46a80c947d44aa28
Jaakko-Heikki Heusala <jheusala@iki.fi>
2015-02-09 08:23:16 +0200

    Support for inplace expanding

--- src/expand.js
+++ src/expand.js
@@ -8,11 +8,21 @@ var merge = require('./merge.js');
 
 /** Scan document for same partial objects and merge them
  * @param doc {object} The document to expand
+ * @param opts {object} The options (optional)
+ * @param opts.inplace {object} Expand document inplace. Does not make new copy.
  * @returns {object} The expanded copy of document
  */
-module.exports = function expand_objects(doc) {
+module.exports = function expand_objects(doc, opts) {
 	debug.assert(doc).is('object');
-	doc = JSON.parse(JSON.stringify(doc));
+	opts = opts || {};
+	debug.assert(opts).is('object');
+
+	var _inplace = opts.inplace ? true : false;
+
+	if(!_inplace) {
+		doc = JSON.parse(JSON.stringify(doc));
+	}
+
 	var cache = scan(doc);
 	ARRAY(Object.keys(cache)).forEach(function(key) {
 		merge(cache[key]);
